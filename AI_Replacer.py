f07db535078d9c1f0cde1ba11f142a373d56d010
dankton96 <dankton96@hotmail.com>
2019-12-19 17:34:55 +0000

    Update AI_Replacer.py
    started implmentation of pattern recognition

--- AI_Replacer.py
+++ AI_Replacer.py
@@ -1,8 +1,7 @@
 import easygui
 numList=['0','1','2','3','4','5','6','7','8','9']
-letterList=['a','b','ç','c','d','e','f','g','i','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+letterList=['.','a','b','c','d','e','f','g','i','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 FilePath=easygui.fileopenbox("Selecione o arquivo a ser manipulado:")
-#print(FilePath)
 file=open(FilePath,'r+')
 sub=[]
 for sc in file:
@@ -10,6 +9,14 @@ for sc in file:
 text=''
 for i in sub:
     text+=str(i)
-print(text)
-set4=[text[i:i+4] for i in text[:-3]]
-print(set4)
+pattern=''
+i=0
+places2add=[]
+for c in text[:-4]:
+    pattern=c+text[i+1]+text[i+2]+text[i+3]
+    if(pattern[0] in numList):
+        if(pattern[-1] in letterList):
+            if('<i' not in pattern):
+                places2add.append(i)
+                print(pattern+'\n')
+    i+=1
