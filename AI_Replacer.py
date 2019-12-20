0b08a01846e99fdd2824ced0d49ba71a7884f124
dankton96 <dankton96@hotmail.com>
2019-12-20 02:32:46 +0000

    Update AI_Replacer.py
    Started implementing correction method. Not working yet.

--- AI_Replacer.py
+++ AI_Replacer.py
@@ -1,6 +1,6 @@
 import easygui
 numList=['0','1','2','3','4','5','6','7','8','9']
-letterList=['.','a','b','c','d','e','f','g','i','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+letterList=['a','b','c','d','e','f','g','i','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 FilePath=easygui.fileopenbox("Selecione o arquivo a ser manipulado:")
 file=open(FilePath,'r+')
 sub=[]
@@ -18,5 +18,11 @@ for c in text[:-4]:
         if(pattern[-1] in letterList):
             if('<i' not in pattern):
                 places2add.append(i)
-                print(pattern+'\n')
+                #print(pattern+'\n')
     i+=1
+for p in places2add:
+    text=text[:p]+'<i>'+text[(p+1):]
+outFile=open("CorrectedSub.srt","w")
+outFile.write(text)
+file.close()
+outFile.close()
